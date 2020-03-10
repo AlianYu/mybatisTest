2c5fe9753ad614f6f514f5a93e02a0bd323c746a
AlinaYu <ylq1349578685@163.com>
2020-03-10 15:51:59 +0800

    1

--- BookShare/src/com/Constants.java
+++
@@ -1,7 +0,0 @@
-package com;
-
-public class Constants {
-    public static final String TIP_MESSAGE ="TIP_MESSAGE";
-    
-
-}
--- BookShare/src/com/book/exception/DBException.java
+++
@@ -1,6 +0,0 @@
-package com.book.exception;
-
-public class DBException extends RuntimeException{
-    
-
-}
